2e2ad6ff45cf9a6e739c181426d23a3bd8469a60
wolpique <pomidorkartoshka@gmail.com>
2023-12-20 20:44:28 +0600

    [dev] auth middleware

--- src/middlewares/auth/auth-middleware.ts
+++ src/middlewares/auth/auth-middleware.ts
@@ -5,20 +5,27 @@ const password = 'qwerty'
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
 const auth = req.headers['authorization'];
+
 if (!auth){
     res.sendStatus(401)
     return
 }
+
     const[basic, token] = auth.split(" ");
+
     if (basic !== "Basic"){
         res.sendStatus(401)
         return
     }
+
     const decodedData = Buffer.from(token, 'base64').toString()
+
     const [decodedLogin, decodedPassword] = decodedData.split(":")
+
     if (decodedLogin !== login || decodedPassword !== password){
         res.sendStatus(401)
         return
     }
+    
     return next()
 }
